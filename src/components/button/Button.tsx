fac694253252e55e430430cae9e5e7e94ebce5a7
Julio Rodríguez <julio10489@gmail.com>
2024-07-12 16:21:05 -0500

    Cambiado: Cambios en el padding, para manjear mejor el responsive

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -12,8 +12,8 @@ export const Button = ({
     <button
       {...props}
       type="button"
-      className={`${primary ? "bg-blue-700 text-white font-bold border-blue-700" : "bg-transparent border-black"} border-2 border-solid rounded-md px-3 py-1 
-      ${size == "small" ? "min-w-12 max-w-16" : size == "medium" ? "min-w-16 max-w-20" : size == "large" ? "min-w-20 max-w-24": "min-w-24"}
+      className={`${primary ? "bg-blue-700 text-white font-bold border-blue-700" : "bg-transparent border-black"} border-2 border-solid rounded-md 
+      ${size == "small" ? "min-w-8 max-w-16 p-0.5" : size == "medium" ? "min-w-16 max-w-20 p-1" : size == "large" ? "min-w-20 max-w-24 px-2 py-1": "min-w-24 px-3 py-1"}
       ${props.className}`}
     >
       {children}
