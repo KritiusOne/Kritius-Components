4edae017d8e5ed21a01a6cb5d82508e3579ea4f7
Julio Rodríguez <julio10489@gmail.com>
2024-07-11 20:35:51 -0500

    Cambiado: Eliminación del componente de prueba Header y su historia, además de cambio de ubicacion del componente button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -13,9 +13,8 @@ export const Button = ({
       {...props}
       type="button"
       className={`${primary ? "bg-blue-700 text-white font-bold border-blue-700" : "bg-transparent border-black"} border-2 border-solid rounded-md px-3 py-1 
-      ${size == "small" ? "min-w-8 max-w-16" : size == "medium" ? "min-w-12 max-w-20" : size == "large" ? "min-w-16 max-w-24": "min-w-20"}
+      ${size == "small" ? "min-w-12 max-w-16" : size == "medium" ? "min-w-16 max-w-20" : size == "large" ? "min-w-20 max-w-24": "min-w-24"}
       ${props.className}`}
-      
     >
       {children}
     </button>
