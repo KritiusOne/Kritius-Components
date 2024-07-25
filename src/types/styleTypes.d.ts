1f027f0981547c4d63775b588ed3cbd52b579a6f
Julio Rodríguez <julio10489@gmail.com>
2024-07-24 21:21:14 -0500

    Agregado: archivo de declaraciones, para poder mejorar la legibilidad del codigo y llegar a un estandard

---
+++ src/types/styleTypes.d.ts
@@ -0,0 +1,12 @@
+export interface Sizes {
+  small: string
+  medium: string
+  large: string
+}
+export interface ColorsSupported {
+  blue: string
+  red: string
+  yellow: string
+  Dark: string
+  Light: string
+}
