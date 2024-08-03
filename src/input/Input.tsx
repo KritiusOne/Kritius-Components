c818e1f6392740774d5d2db88978c7ef4e3c0759
Julio Rodríguez <julio10489@gmail.com>
2024-08-03 09:47:31 -0500

    Cambiado: Transparencia del bg al input cuando es uniqueBorder

--- src/input/Input.tsx
+++ src/input/Input.tsx
@@ -20,11 +20,11 @@ const getColorClasses = (uniqueBorder: boolean, inputColor: keyof ColorsSupporte
     blue: 'focus:border-blue-700 focus:placeholder-blue-700 hover:border-slate-900',
     red: 'focus:border-red-700 focus:placeholder-red-700 hover:border-slate-900',
     yellow: 'focus:border-yellow-400 focus:placeholder-yellow-400 hover:border-slate-900',
-    Dark: 'focus:border-slate-700 focus:placeholder-white hover:border-black',
+    Dark: 'focus:border-slate-700 focus:placeholder-grey-500 hover:border-black',
     Light: 'focus:border-white focus:placeholder-grey-500 hover:border-slate-500',
   };
 
-  return uniqueBorder ? `border-b-solid border-b-2 ${inputUniqueBorder[inputColor]}` : `border-2 border ${baseClasses[inputColor]}`;
+  return uniqueBorder ? `bg-transparent border-b-solid border-b-2 ${inputUniqueBorder[inputColor]}` : `border-2 border ${baseClasses[inputColor]}`;
 };
 export const Input: React.FC<Props> = ({uniqueBorder = false, inputColor = "Dark",...props})=>{
   const inputClass = useMemo(()=> classNames(`placeholder:text-sm outline-none px-2 py-1 rounded-sm w-full border-slate-300 ${inputColor == "Light" ? "bg-transparent hover:border-yellow-200 text-white focus:border-yellow-500": "bg-slate-100  hover:border-slate-700"}  ${getColorClasses(uniqueBorder, inputColor)}`), [inputColor, uniqueBorder])
